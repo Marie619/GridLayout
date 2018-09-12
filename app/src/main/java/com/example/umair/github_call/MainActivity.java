a8482140e1a33c1b2c729e40455974c306ee9ae2
Umair <umairmajeed719@gmail.com>
2018-09-12 17:15:53 +0500

    commited new file

--- app/src/main/java/com/example/umair/github_call/MainActivity.java
+++ app/src/main/java/com/example/umair/github_call/MainActivity.java
@@ -19,19 +19,34 @@ List<ProfileModel> profileModels;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mRecView=(RecyclerView)findViewById(R.id.mRecyclerView);
-       // LinearLayoutManager layoutManager=new LinearLayoutManager(this);
+        LinearLayoutManager layoutManager=new LinearLayoutManager(this);
 
-        mRecView.setLayoutManager(new GridLayoutManager(this,2));
+        mRecView.setLayoutManager(layoutManager);
         mRecView.setHasFixedSize(true);
 
         profileModels=new ArrayList<>();
 
-        ProfileModel model=new ProfileModel(R.drawable.ic_launcher_background,"Ali");
-        ProfileModel model1=new ProfileModel(R.drawable.ic_launcher_background,"Asad");
-        ProfileModel model2=new ProfileModel(R.drawable.ic_launcher_background,"Akraam");
+        ProfileModel model=new ProfileModel(R.drawable.pakistan,"Pakistan");
+        ProfileModel model1=new ProfileModel(R.drawable.america,"America");
+        ProfileModel model2=new ProfileModel(R.drawable.australia,"Australia");
+        ProfileModel model3=new ProfileModel(R.drawable.canada,"Canada");
+        ProfileModel model4=new ProfileModel(R.drawable.england,"England");
+        ProfileModel model5=new ProfileModel(R.drawable.germany,"Germany");
+        ProfileModel model6=new ProfileModel(R.drawable.iceland,"Iceland");
+        ProfileModel model7=new ProfileModel(R.drawable.india,"India");
+        ProfileModel model8=new ProfileModel(R.drawable.ukraine,"ukraine");
+        ProfileModel model9=new ProfileModel(R.drawable.turkey,"Turkey");
+
         profileModels.add(model);
         profileModels.add(model2);
         profileModels.add(model1);
+        profileModels.add(model3);
+        profileModels.add(model4);
+        profileModels.add(model5);
+        profileModels.add(model6);
+        profileModels.add(model7);
+        profileModels.add(model8);
+        profileModels.add(model9);
         viewAdapter=new MRecyclerViewAdapter(profileModels);
         mRecView.setAdapter(viewAdapter);
 
